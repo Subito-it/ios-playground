fd4c0774d87673e9878c21577cfde167488b88ce
Faber92 <dogger56@hotmail.it>
2025-12-02 18:18:52 +0100

    Change background color

--- Playground/ViewController.swift
+++ Playground/ViewController.swift
@@ -6,6 +6,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBlue
     }
 }
