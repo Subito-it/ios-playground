56dbe56a83fb33ede7c7533a9fe015c4db0d1a07
Tomas Camin <tomas.camin@adevinta.com>
2025-12-02 17:30:49 +0100

    Initial commit

---
+++ Playground/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Playground
+//
+//  Created by Tomas Camin on 02/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
